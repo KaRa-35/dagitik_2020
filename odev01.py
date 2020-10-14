ca8056355b5e3e4119581d7901ec186a8496ea66
KaRa-35 <37421715+KaRa-35@users.noreply.github.com>
2020-10-14 15:29:20 -0400

    Update odev01.py

--- odev01.py
+++ odev01.py
@@ -1,1 +1,20 @@
+import sys
+
+entries = []
+class addEntry:
+    def _init_(self, name, surname, age):
+        self.name = name
+        self.surname = surname
+        self.age = age
+
+N = int(input("kaç kisi"))
+for i in range(N):
+    input = input("bilgileri giriniz")
+    entries.append(addEntry(input[0:input.find(" ")], input[input.find(" "):input.find(" ", input.find(" "))],
+                            input[input.find(" ", input.find(" "))]))
+
+
+if (len(sys.argv) <2):
+
+    sys.exit ()
 
